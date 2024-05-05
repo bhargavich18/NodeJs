043399a306a0a02179e764fcdbf8d4e832c076bb
unknown <bhargavichiluka1898@gmail.com>
2024-05-05 16:38:39 +0530

    Nodejs project

---
+++ ReadMe copy.md
@@ -0,0 +1,23 @@
+### Simple NodeJS Fastify App
+
+#### Resources
+
+This Project comes with a set of 6 common resources:
+
+- `/posts` 100 posts
+- `/comments` 500 comments
+- `/albums` 100 albums
+- `/photos` 5000 photos
+- `/todos` 200 todos
+- `/users` 10 users
+
+#### Routes
+
+All HTTP methods are supported. You can use http or https for your requests.
+
+- GET `/posts`
+- GET `/posts/1`
+- GET `/posts/1/comments`
+- GET `/comments?postId=1`
+
+> Note: This server doesn't store any data and fetches the original content from JSONPlaceholder TypeServer.
---
+++ server.js
@@ -0,0 +1,62 @@
+// Import the framework and instantiate it
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+const ORIGINAL_SERVER = "https://jsonplaceholder.typicode.com";
+
+const getData = async (url) => {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    fastify.log.error(error);
+    return {
+      message: "Error something went wrong",
+    };
+  }
+};
+
+// Declare a route
+fastify.get("/", async function handler(request, reply) {
+  return { hello: "world" };
+});
+
+fastify.get("/posts/:id?", async (request, reply) => {
+  if (request.params.id) {
+    return getData(`${ORIGINAL_SERVER}/posts//${request.params.id}`);
+  } else {
+    return getData(`${ORIGINAL_SERVER}/posts`);
+  }
+});
+
+fastify.get("/posts/:id/comments", async (request, reply) => {
+  return getData(`${ORIGINAL_SERVER}/posts//${request.params.id}/comments`);
+});
+fastify.get("/comments", async (request, reply) => {
+  if (request.query.postId) {
+    return getData(
+      `${ORIGINAL_SERVER}/comments?postId=${request.query.postId}`
+    );
+  } else {
+    return getData(`${ORIGINAL_SERVER}/comments`);
+  }
+});
+fastify.get("/albums", async (request, reply) => {
+  return getData(`${ORIGINAL_SERVER}/albums`);
+});
+fastify.get("/todos", async (request, reply) => {
+  return getData(`${ORIGINAL_SERVER}/todos`);
+});
+fastify.get("/users", async (request, reply) => {
+  return getData(`${ORIGINAL_SERVER}/users`);
+});
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
